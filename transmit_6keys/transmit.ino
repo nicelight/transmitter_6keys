f3c34851e031e39659771ce604eca7bdfedc6f75
Sergio <harrizmat@mail.ru>
2018-12-18 14:22:55 +0500

    transmitter added
    jsut simle hello world

---
+++ transmit_6keys/transmit.ino
@@ -0,0 +1,17 @@
+// проверь как будет работать с раскомментированной шестой строкой 
+
+void transmit() {
+  digitalWrite(13, 1);
+  const char *msg1 = "Hello kitty ";  //  "Привет, мир!"
+  //const char *msg1 = "Hello kitty " + counter;  //  "Привет, мир! mod"
+  driver.send((uint8_t *)msg1, strlen(msg1));
+  driver.waitPacketSent();
+  delay(100);
+  counter++;
+  driver.send(counter, 2);
+  driver.waitPacketSent();
+  delay(100);
+  digitalWrite(13, 0);
+
+}//void transmit()
+
---
+++ transmit_6keys/transmit_6keys.ino
@@ -0,0 +1,24 @@
+/// RF передатчик
+/// сигнальный провод с передатчика на пин 12 arduino nano
+#include <RH_ASK.h>
+#include <SPI.h>  //  в скетче не используется,
+//  но нужна для компиляции
+
+RH_ASK driver;
+byte counter = 0; // тестовая переменная, считалка
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  Serial.println("Trying init..");
+  while (!driver.init()) {
+    Serial.println("init failed");  //  "Инициализация не удалась"
+    delay(500);
+  }
+  Serial.println("RF transmit init done.");
+}//setup
+
+void loop() {
+  transmit();
+  delay(1000);
+}//loop
