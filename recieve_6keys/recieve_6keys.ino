14d4061baacf3068be58e6d161c073f9172a225d
Sergio <harrizmat@mail.ru>
2018-12-18 14:21:04 +0500

    First uploading of code
    Just simple example without any businneslogic

---
+++ recieve_6keys/recieve_6keys.ino
@@ -0,0 +1,22 @@
+/// RF приемник
+/// сигнальный провод с приемника на пин 11 arduino nano
+
+#include <RH_ASK.h>
+#include <SPI.h> // в скетче не используется, но нужна для компиляции
+
+RH_ASK driver;
+
+void setup() {
+  Serial.begin(115200); // только в отладочных целях
+
+  Serial.println("Trying init..");
+  while (!driver.init()) {
+    Serial.println("init failed");  //  "Инициализация не удалась"
+    delay(500);
+  }
+  Serial.println("RF reciever init done.");
+}//setup
+
+void loop() {
+  recieve();
+}// loop
---
+++ recieve_6keys/recieving.ino
@@ -0,0 +1,13 @@
+
+void recieve(){
+    uint8_t buf[12];
+  uint8_t buflen = sizeof(buf);
+  if (driver.recv(buf, &buflen)) { // неблокирующая функция
+    int i;
+    // пришло сообщение с хорошей контрольной суммой,
+    // выгружаем его:
+    Serial.print("recieved: ");  //  "Принято Сообщение: "
+    Serial.println((char*)buf);
+  }// if recieve
+}//void recieve()
+
